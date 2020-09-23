d09ac071e6d3f5e30d22a43e0f2a7035a4f1b660
Sta-sky <56905658+Sta-sky@users.noreply.github.com>
2020-09-23 17:20:55 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,5 +1,12 @@
 import IPy
 
+import time
+import jwt
+from tool_script.byte_change_str import change_str
+
+SALT_KEY = 'PXU9@ctuNov20!'
+
+
 def is_ipv4_address(ip):
     if ip:
         try:
@@ -10,5 +17,15 @@ def is_ipv4_address(ip):
             print(e, 'cdsaca=====')
             return False
 
-ip = '535'
-is_ipv4_address(ip)
+
+def generate_token(username, exp=3600 * 24):
+    """
+    过期时间为一天
+    :param salt: 加盐值
+    :param exp: 过期时间
+    :return:
+    """
+    key = SALT_KEY
+    now = time.time()
+    payload = {'username': username, 'exp': exp + now}
+    return change_str(jwt.encode(payload=payload, key=key, algorithm='HS256'))
