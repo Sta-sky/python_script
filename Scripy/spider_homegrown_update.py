b3c5a399fe5fa29f61c0f1228c14f40750fce067
Sta-sky <56905658+Sta-sky@users.noreply.github.com>
2020-10-23 16:34:08 +0800

    Update spider_homegrown_update.py

--- Scripy/spider_homegrown_update.py
+++ Scripy/spider_homegrown_update.py
@@ -1,14 +1,13 @@
 import os
-import random
 import re
 import time
-from threading import Thread, Lock
 import queue
-
-import requests
-from Crypto.Cipher import AES
-from lxml import etree
 import urllib3
+import random
+import requests
+from lxml import etree
+from Crypto.Cipher import AES
+from threading import Thread, Lock
 
 
 class SpiderVideo(object):
@@ -28,9 +27,8 @@ class SpiderVideo(object):
         self.url_queue = queue.Queue(1000)
 
     def get_video_id(self):
-        href_list = []
+        re_href_list = []
         href_count = 0
-        totle = (self.end_page - self.start_page) * 12
         try:
             for page in range(self.start_page, self.end_page):
                 request_res = self.return_requests_data(
@@ -39,22 +37,28 @@ class SpiderVideo(object):
                     html_data = request_res
                     element = etree.HTML(html_data)
                     href_data = element.xpath('//a/@href')
-                    for href in href_data:
-                        if href.split('.')[-1] == 'html':
-                            if href in href_list:
-                                continue
-                            href_list.append(href)
-                            href_count += 1
-                            print(f'总共{totle}个href，正在获取第{href_count}个，href为{href}')
+                    re_href_list, href_count = self.get_href_lilst(
+                        href_data, href_count, re_href_list)
                     time.sleep(1)
                 else:
                     print('==主页面==地址，请求失败了， 开始请求下个页面')
                     continue
-            return href_list
+            return re_href_list
         except Exception as e:
             print(f'id列表获取失败，失败原因为{e}')
         finally:
-            return href_list
+            return re_href_list
+
+    def get_href_lilst(self, href_data, href_count, href_list):
+        totle = (self.end_page - self.start_page) * 12
+        for href in href_data:
+            if href.split('.')[-1] == 'html':
+                if href in href_list:
+                    continue
+                href_list.append(href)
+                href_count += 1
+                print(f'总共{totle}个href，正在获取第{href_count}个，href为{href}')
+        return href_list, href_count
 
     def get_m3u8_save_video(self, link, th_name, th_status):
         print(f'当前视频的链接地址为{link}')
@@ -63,12 +67,11 @@ class SpiderVideo(object):
             page_data = request_res
             m3u8_re = '(.*)\.m3u8'
             src_path = re.findall(m3u8_re, page_data)
-            print('文件中匹配出的没有过滤的m3u8地址列表', src_path)
+            # print('文件中匹配出的没有过滤的m3u8地址列表', src_path)
             if not src_path:
                 print('匹配出的m3u8列表为空，下载下一个')
                 return False
             m3u8_file_res_url = src_path[2].split(': ')[1].split('"')[1]
-            print(m3u8_file_res_url)
             ele = etree.HTML(page_data)
             try:
                 file_name = ele.xpath('//title/text()')[0]
@@ -85,22 +88,21 @@ class SpiderVideo(object):
             request_res = self.handle_m3u8_ts_key(m3u8_file_res_url)
             if request_res:
                 re_ts_list, re_ts_base_url, re_key_base_url = request_res
-    
+
                 key_val, ts_list_url = self.request_ts_key_url_list(
                     re_ts_list, re_ts_base_url, re_key_base_url)
-        
+
                 ts_length = len(ts_list_url)
                 if ts_length > self.ts_list_size:
                     print(f'文件ts数量为{ts_length}个，大于1500个ts，跳过，等待下载下一个')
                     return False
-                return key_val, ts_list_url, file_name, 
+                return key_val, ts_list_url, file_name,
             else:
                 return False
         else:
             print('==子页面==地址，请求失败了， 开始请求下个页面')
             return False
-            
-            
+
     def handle_m3u8_ts_key(self, m3u8_file_res_url):
         # 从页面上匹配的m3u8地址没有https：前缀，这种情况是在有需要key解密的ts，
         judge_http = m3u8_file_res_url.split('/')
@@ -124,7 +126,7 @@ class SpiderVideo(object):
             m3u8_re = '(.*)\.m3u8'
             true_link = re.findall(m3u8_re, false_m3u8_file)
             print('匹配出的m3u8链接列表为：', true_link)
-    
+
             # 如果是两个m3u8 url，则拼接真实的m3u8 url，获取ts文件，
             # 如果不是，则说明只有一个m3u8 url，继续使用上面的false_m3u_url获取的数据匹配ts文件
             if true_link:
@@ -169,20 +171,18 @@ class SpiderVideo(object):
         for ts in ts_lists:
             res_ts_url = ts_base_url + str(ts) + '.ts'
             ts_url_list.append(res_ts_url)
-
         if key_url_base:
             key_url = key_url_base + '.key'
             print(f'文件需要解密，密钥url为{key_url}')
             key_banery = self.return_requests_data(key_url)
             key_banerys = key_banery.content
             key_banery.close()
-
         else:
             print('文件不需要解密')
             key_banerys = None
         return key_banerys, ts_url_list
 
-    def decrypt_save_ts(self, key, ts_urls, new_save_path, file_name, 
+    def decrypt_save_ts(self, key, ts_urls, new_save_path, file_name,
                         down_th_name):
         """功能函数2:解密,保存"""
 
@@ -192,7 +192,8 @@ class SpiderVideo(object):
         for ts_url in ts_urls:
             down_num += 1
             progrees = float('%.2f' % (down_num / total_size)) * 100
-            print('ts数量{}，当前下载第{}个，已经下载{}%,'.format(total_size, down_num,progrees))
+            print('ts数量{}，当前下载第{}个，已经下载{}%,'.format(total_size, down_num,
+                                                    progrees))
             ts_name = ts_url.split("/")[-1]  # ts文件名
             # 解密，new有三个参数，
             # 第一个是秘钥（key）的二进制数据，
@@ -234,7 +235,8 @@ class SpiderVideo(object):
             try:
                 if retry_count > 1:
                     print(f'重试第{retry_count - 1}次请求，当前请求地址为{param_url}请等待...')
-                http_res = requests.get(url=param_url, verify=False,timeout=10)
+                http_res = requests.get(url=param_url, verify=False,
+                                        timeout=10)
                 return http_res
             except Exception as e:
                 if retry_count >= retry_times:
@@ -261,21 +263,29 @@ class MySpiderThread(Thread, SpiderVideo):
     def run(self):
         down_count_video = 0
         while True:
-            down_count_video += 1
-            link = self.url_queue.get()
-            print(f'由=={self.name}==开始下载第{down_count_video}个视频')
-            request_res = self.get_m3u8_save_video(link, self.name, self.alive_staatus)
-            if not request_res:
-                continue
+            if self.url_queue.empty():
+                down_count_video += 1
+                link = self.url_queue.get()
+                print(f'由=={self.name}==开始下载第{down_count_video}个视频')
+                request_res = self.get_m3u8_save_video(
+                    link, self.name, self.alive_staatus)
+                if not request_res:
+                    continue
+                else:
+                    key, ts_urls, new_save_path, file_name = request_res
+                    self.decrypt_save_ts(key, ts_urls, new_save_path,
+                                         file_name,
+                                         self.name)
+                self.lock.acquire()
             else:
-                key, ts_urls, new_save_path, file_name = request_res
-                self.decrypt_save_ts(key, ts_urls, new_save_path, file_name,
-                                     self.name)
-            self.lock.acquire()
+                print('所有视频下载完成，队列为空')
+                break
 
 
 if __name__ == '__main__':
-    
+    """
+    多线程下载视频
+    """
     for num in range(5):
-        th_name = '党_的线程_' + str(num) 
+        th_name = '党_的线程_' + str(num)
         my_th = MySpiderThread(th_name)
